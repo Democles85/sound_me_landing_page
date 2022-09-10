07012335dec620a7ae3fbbdafd10bc28b5b068a9
Sixhei Tartari <democleschannel28@gmail.com>
2022-09-10 20:49:21 +0200

    ✏️ Updated Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,13 @@ import { Box, Container, Text } from '@chakra-ui/react'
 export default function Footer() {
   return (
     <Container maxW={'100%'}>
-      <Box align={'center'} opacity={'0.5'} fontSize={'sm'}>
+      <Box
+        display={'flex'}
+        flexDir={'column'}
+        textAlign={'center'}
+        opacity={'0.5'}
+        fontSize={'sm'}
+      >
         <Text>{new Date().getFullYear()} &copy; Sound Me</Text>
         <Text>All rights reserved.</Text>
       </Box>
